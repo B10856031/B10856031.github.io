f23ca03baf700e1a9d5e3b08c98d191ef695e88f
B10856031 <64625166+B10856031@users.noreply.github.com>
2020-07-01 09:22:07 +0800

    Add files via upload

---
+++ C 程設/0000/2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int i,j,num;
+	printf("請輸入一個數：");
+	scanf("%d",&num);
+	for(i=0;i<num;i++){
+		for(j=0;j<i;j++){
+			printf(" ");
+		}
+		for(j=0;j<num-i;j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
